30f61fda3d0616f9f5f395468d0633992200c8b6
vinciraia99 <99raia99@gmail.com>
2022-12-12 09:44:25 +0100

    add fix no sandbox for root user linux

--- utility.py
+++ utility.py
@@ -41,6 +41,7 @@ def setOption():
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-logging')
+    options.add_argument('--no-sandbox')
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
     return options
 
